40647d021a079ef1231d155c9df5f8a8e170f615
Shuvo Kumer Ray <sk.ray1998t@gmail.com>
2025-07-16 00:22:02 +0600

    mongo_uri added

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const { spawn } = require('child_process');
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
 const app = express();
