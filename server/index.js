33728f76e755f05fedf8feaa4bc136f476783ad6
Shuvo Kumer Ray <sk.ray1998t@gmail.com>
2025-07-15 23:51:30 +0600

    port added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const multer = require('multer');
 const fs = require('fs');
 const { spawn } = require('child_process');
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
